dffe7417dd38b5cb4286da9e95b3a7875640dd6d
main-2017 <agustinducca@maingroup.co>
2018-11-14 11:32:15 -0300

    DbContext configure and AngusInspect created

---
+++ QuarterControl/Models/AngusInspect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterControl.Models
+{
+    public class AngusInspect : Inspect
+    {
+        public bool MarmoreoApto { get; set; }
+
+        public override bool Aprueba()
+        {
+            return MarmoreoApto;
+        }
+    }
+}
---
+++ QuarterControl/Models/DB/QuarterControlDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterControl.Models.DB
+{
+    public class QuarterControlDBContext : DbContext 
+    {
+        public DbSet<AngusInspect> AngusInspects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AngusInspect>().ToTable("AngusInspect");
+        }
+    }
+}
