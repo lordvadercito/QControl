5043370b885f19bbfd0124097cb872c0f34372e7
main-2017 <agustinducca@maingroup.co>
2018-11-14 10:38:39 -0300

    Creating scaffolding

---
+++ QuarterControl/Models/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterControl.Models
+{
+    public class Entity
+    {
+        [Display(Name = "Creado")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        [Display(Name = "Actualizado")]
+        public DateTime Updated { get; set; } = DateTime.Now;
+    }
+}
---
+++ QuarterControl/Models/Inspect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterControl.Models
+{
+    public abstract class Inspect : Entity
+    {
+        int GarronID { get; set; }
+
+        public abstract bool Aprueba();
+    }
+}
