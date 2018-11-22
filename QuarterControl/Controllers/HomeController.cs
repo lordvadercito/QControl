b33225db7da8b0e839a50c44299e867b229e867d
main-2017 <agustinducca@maingroup.co>
2018-11-22 18:35:45 -0300

    first functional MVP

--- QuarterControl/Controllers/HomeController.cs
+++ QuarterControl/Controllers/HomeController.cs
@@ -29,7 +29,11 @@ namespace QuarterControl.Controllers
             return View(displayInitial);
         }
 
-        //Busca el garrón en función del codbar ingresado
+        /// <summary>
+        /// Busca el garrón en función del codbar ingresado
+        /// </summary>
+        /// <param name="codbar">Código de barras del garrón pickeado</param>
+        /// <returns>Redirige a la vista apropiada</returns>
         public ActionResult SearchCodbar(string codbar)
         {
             RepositoryControllers repository = new RepositoryControllers(_context);
@@ -38,13 +42,32 @@ namespace QuarterControl.Controllers
                 repository = repository.GetInformation(codbar)
             };
 
-            repositoryGarron.Codbar = codbar;
-            repositoryGarron.networkStatus = netstat.NetworkUp();
+            bool queryCodbar = _context.AngusInspects.Any(x => x.GarronID == repositoryGarron.repository.GarronId);
 
-            return View(repositoryGarron);
+
+            if (!queryCodbar)
+            {
+                repositoryGarron.Codbar = codbar;
+                repositoryGarron.networkStatus = netstat.NetworkUp();
+
+                return View(repositoryGarron);
+            }
+            else
+            {
+                int state = 0;
+                string error = "El codbar ya fue procesado";
+
+                return RedirectToAction("ResultOperation", new { state, errorMessage = error});
+            }
+
         }
 
-        //Guarda el valor del marmoreo
+        /// <summary>
+        /// Guarda el valor del marmoreo en la base AngusInspect
+        /// </summary>
+        /// <param name="garronId">Int ID del garrón en la base PalcoDevesaTest</param>
+        /// <param name="marmoreoApto">String Valor del marmoreo (Apto o No apto)</param>
+        /// <returns></returns>
         public ActionResult SaveMarmoreo(int garronId, string marmoreoApto)
         {
             AngusInspect garron = new AngusInspect();
@@ -58,40 +81,51 @@ namespace QuarterControl.Controllers
                 garron.MarmoreoApto = false;
             }
 
-            var queryCodbar = from garronCodbar in _context.AngusInspects
-                              where garronCodbar.GarronID == garronId
-                              select garronId;
-
             int state = 0;
             string error = null;
+            int errorCode = 0;
 
-            if (queryCodbar.Count() == 0)
+            _context.AngusInspects.Add(garron);
+
+            try
             {
-                _context.AngusInspects.Add(garron);
-               
-                try
-                {
-                    state = _context.SaveChanges();
-                }
-                catch (Exception ex)
-                {
-                    error = ex.Message;
+                state = _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                errorCode = ex.HResult;
+            }
+             
+            return RedirectToAction("ResultOperation", new { state, errorMessage = error, errorCode});
 
-                }
+
+
+        }
+
+        /// <summary>
+        /// Redirecciona a la vista de error o sucess según corresponda
+        /// </summary>
+        /// <param name="state">Estado de la operación (1 = Exitosa)</param>
+        /// <param name="errorMessage">Descripción de la excepción</param>
+        /// <param name="errorCode">Código del error</param>
+        /// <returns>Vista correspondiente</returns>
+        public ActionResult ResultOperation(int state, string errorMessage, int errorCode)
+        {
+            if(state == 1)
+            {
+                return View("Success");
             }
             else
             {
-                error = "El codbar ya fue procesado";
+                Error error = new Error
+                {
+                    ErrorMessage = errorMessage,
+                    ErrorCode = errorCode
+            };
+                return View("Error", error);
             }
-
             
-
-            ViewBag.State = state;
-            ViewBag.Error = error;
-            return View();
-            
-            
-
         }
 
     }
---
+++ QuarterControl/Models/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterControl.Models
+{
+    public class Error
+    {
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
